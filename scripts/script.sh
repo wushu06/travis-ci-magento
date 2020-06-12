b756b524a02d20253288d496f7da583eb8920866
nourlatreche <nour@elementarydigital.co.uk>
2020-06-12 19:19:49 +0100

    switch to functional

--- scripts/script.sh
+++ scripts/script.sh
@@ -4,9 +4,10 @@ set -e
 trap '>&2 echo Error: Command \`$BASH_COMMAND\` on line $LINENO failed with exit code $?' ERR
 
 SCRIPT_STEP_SCRIPTS_PATH="$(pwd)/scripts/steps/script"
-
+echo 'Running unit test';
 ./vendor/phpunit/phpunit/phpunit  app/code/Elementary/EmployeesManager/Test/Unit/Model/CustomerEmployee/TestModel.php
-
+echo 'Functional test...';
+./vendor/bin/mftf run:test AdminLoginTest --remove
 
 #cd $MAGENTO_DIR
 #${SCRIPT_STEP_SCRIPTS_PATH}/phpcs.sh
